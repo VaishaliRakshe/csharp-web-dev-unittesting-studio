8b936362abc47c4315a1091d267dea22abdaced7
VaishaliRakshe <vaishalijadhav000@gmail.com>
2021-12-01 10:22:12 -0600

    update

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -9,9 +9,40 @@ namespace BalancedBracketsTests
         // TODO: Add tests to this file.
 
         [TestMethod]
+
         public void EmptyTest()
         {
             Assert.AreEqual(true, true);
+        }
+        [TestMethod]
+        public void TestLeftAndRight()
+        {
+            Assert.IsTrue(BalancedBracketsTests.HasBalancedBrackets("[]"));
+        }
+        [TestMethod]
+        public void TestRightBracketReturnFalse()
+        {
+            Assert.IsFalse(BalancedBracketsTests.HasBalancedBrackets("]"));
+        }
+        [TestMethod]
+        public void TestLeftBracketReturnFalse()
+        {
+            Assert.IsFalse(BalancedBracketsTests.HasBalancedBrackets("["));
+        }
+        [TestMethod]
+        public void TestTextLeftBracketTextRightBracket()
+        {
+            Assert.IsTrue(BalancedBracketsTests.HasBalancedBrackets("Java[Script]"));
+        }
+        [TestMethod]
+        public void TestBackwardBracket()
+        {
+            Assert.IsFalse(BalancedBracketsTests.HasBalancedBrackets("]["));
+        }
+        [TestMethod]
+        public void TestThreeLeftBracketsAndThreeRight()
+        {
+            Assert.IsFalse(BalancedBracketsTests.HasBalancedBrackets("]]]JavaScript[[["));
         }
     }
 }
